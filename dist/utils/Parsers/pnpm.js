83994763979654e8abed4be4ceb4bb23057aca42
Tariq Yunusa <tariq.yunusa@miirosoftware.com>
2025-09-19 11:51:35 +0100

    fix: pnpm only showing installed dependencies

--- dist/utils/Parsers/pnpm.js
+++ dist/utils/Parsers/pnpm.js
@@ -3,12 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.parsePnpm = void 0;
 const fs_1 = require("fs");
 const path_1 = require("path");
-const yaml_1 = require("yaml");
 const parsePnpm = (projectPath) => {
-    const filePath = (0, path_1.join)(projectPath, 'pnpm-lock.yaml');
-    const raw = (0, fs_1.readFileSync)(filePath, 'utf-8');
-    const parsed = (0, yaml_1.parse)(raw);
-    return Object.keys(parsed.packages || {});
+    const pkgPath = (0, path_1.join)(projectPath, 'package.json');
+    const raw = (0, fs_1.readFileSync)(pkgPath, 'utf-8');
+    const pkg = JSON.parse(raw);
+    return [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.devDependencies || {})];
 };
 exports.parsePnpm = parsePnpm;
 //# sourceMappingURL=pnpm.js.map
