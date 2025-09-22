c323be44a42a0569db985898c0ed781e6e5f80bf
Tariq Yunusa <tariq.yunusa@miirosoftware.com>
2025-09-22 14:02:03 +0100

    fix

--- dist/utils/Parsers/bun.js
+++ dist/utils/Parsers/bun.js
@@ -1,12 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseBun = void 0;
-const child_process_1 = require("child_process");
+const fs_1 = require("fs");
+const path_1 = require("path");
 const parseBun = (projectPath) => {
     try {
-        const output = (0, child_process_1.execSync)('bun pm ls --json', { cwd: projectPath });
-        const parsed = JSON.parse(output.toString());
-        return parsed.map((dep) => dep.name);
+        const pkgPath = (0, path_1.join)(projectPath, 'package.json');
+        const raw = (0, fs_1.readFileSync)(pkgPath, 'utf-8');
+        const pkg = JSON.parse(raw);
+        const deps = {
+            ...pkg.dependencies,
+            ...pkg.devDependencies,
+            ...pkg.peerDependencies,
+            ...pkg.optionalDependencies,
+        };
+        return Object.keys(deps);
     }
     catch (e) {
         console.error('Error parsing Bun dependencies:', e);
